4519087c39dee133af548066998652d03ad82457
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-09-21 16:07:36 -0300

    adicionando subnets

--- infra-aws/vpc.tf
+++ infra-aws/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "new-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "cursoterraform-vpc"
+    Name = "${var.prefix}-vpc"
   }
 }
 
@@ -9,6 +9,17 @@ data "aws_availability_zones" "available" {}
 
 output "all_availability_zones" {
   value = data.aws_availability_zones.available.names  # Lista todas as zonas de disponibilidade disponíveis
+}
+
+resource "aws_subnet" "subnets" {
+  count = 2
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id = aws_vpc.new-vpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.prefix}-subnet-${count.index}"
+  }
 }
 
 
